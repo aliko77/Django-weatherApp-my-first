f3a9a91ff28d8e8edc92d3ec0c3d1fb8be418046
aliko77 <akoc3273@gmail.com>
2023-02-20 09:08:02 +0300

    wow

--- main/views.py
+++ main/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from .functions.location import Location
+import requests
 
 # Create your views here.
 
 
 class index(View):
-    def get(self, request):
+    async def get(self, request):
+        location = Location.getUserLocation()
+        APIKEY = '7c8796e389e4f40e0e76f0079d4b51a7'
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=7c8796e389e4f40e0e76f0079d4b51a7'
+        city = location
+        city_weather = requests.get(url.format(city)).json()
+
         context = {
-            'location': Location.getUserLocation()
+            'location': location,
+            'temperature': city_weather
         }
         return render(
             request=request,
