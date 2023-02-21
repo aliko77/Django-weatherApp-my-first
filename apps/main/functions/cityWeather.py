72a52c70e79b3bb80838011bdbc8427139cfee6d
ali <51881653+aliko77@users.noreply.github.com>
2023-02-21 22:21:52 +0300

    Update cityWeather.py

--- apps/main/functions/cityWeather.py
+++ apps/main/functions/cityWeather.py
@@ -7,7 +7,7 @@ def fahToCel(temp):
 
 class CityWeather:
     def weather(city):
-        APIKEY = '7c8796e389e4f40e0e76f0079d4b51a7'
+        APIKEY = 'APIKEY'
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&lang=tr&appid=' + APIKEY
         cityWeather = requests.get(url.format(city)).json()
         if cityWeather['cod'] == '404':
